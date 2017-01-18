cb7a375f1db78bfc27792fb7dfd4c8b42901579f
Alan Zhang <gagazcfan@icloud.com>
2017-01-18 13:58:44 +0800

    customer info form. payment is finished too, but currently in my personal repo. will merge to here later

--- src/router_views/Customer/Info/script.ts
+++ src/router_views/Customer/Info/script.ts
@@ -1,24 +1,12 @@
 import { Component } from 'vue'
 import { mapGetters, mapActions } from 'vuex'
+import { GET_CUSTOMER } from 'src/infrastructure/store/auth_types'
 
 export default {
   name: 'Info',
   computed: {
     ...mapGetters({
+      customer: GET_CUSTOMER
     })
-  },
-
-  created(this: Component) {
-  },
-
-  watch: {
-  },
-
-  methods: {
-    ...mapActions({
-    })
-  },
-
-  components: {
   }
 }
