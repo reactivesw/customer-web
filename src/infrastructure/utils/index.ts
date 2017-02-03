12ea8ac1f2be387447f4720a63c3a4cb73a8311d
Alan Zhang <gagazcfan@icloud.com>
2017-02-03 15:46:35 +0800

    fix style

--- src/infrastructure/utils/index.ts
+++ src/infrastructure/utils/index.ts
@@ -14,7 +14,7 @@ export function MoneyToString (priceObj) {
 }
 
 export function parseJwt(token) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace('-', '+').replace('_', '/');
-  return JSON.parse(atob(base64));
+  const base64Url = token.split('.')[1]
+  const base64 = base64Url.replace('-', '+').replace('_', '/')
+  return JSON.parse(atob(base64))
 }
