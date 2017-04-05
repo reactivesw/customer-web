b892e7b174fdbd3c653f58cc56f7ae41134fb8c5
Alan Zhang <gagazcfan@icloud.com>
2017-04-05 15:40:19 +0800

    fix vue.d.ts

--- typings/vue.d.ts
+++ typings/vue.d.ts
@@ -4,14 +4,14 @@
  * Assume any import statements started with 'src/router_views' or 'src/components' are vue components,
  * tsc will't analysis source code since we 'declare' those modules.
  */
-import Vue = require('vue')
-
 declare module 'src/router_views/*' {
+  import Vue = require('vue')
   const value: Vue.ComponentOptions<Vue>
   export default value
 }
 
 declare module 'src/components/*' {
+  import Vue = require('vue')
   const value: Vue.ComponentOptions<Vue>
   export default value
 }
