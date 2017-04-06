5aea0d5110d5cff4395c56713fefa06d86d94878
Alan Zhang <gagazcfan@icloud.com>
2017-04-06 17:21:14 +0800

    Header show cart lineitems quantity

--- src/components/TheHeader/script.ts
+++ src/components/TheHeader/script.ts
@@ -3,6 +3,7 @@ import { mapActions, mapGetters } from 'vuex'
 import LanguageSelector from './LanguageSelector'
 import { SHOW_SIGN_IN } from 'src/infrastructure/store/modal_dialogs_types'
 import { GET_CUSTOMER, GET_IS_LOGGED_IN } from 'src/infrastructure/store/modules/auth/getters'
+import { GET_CART } from 'src/infrastructure/store/modules/carts/getters'
 
 export default {
   name: 'TheHeader',
@@ -10,7 +11,8 @@ export default {
   computed: {
     ...mapGetters({
       customer: GET_CUSTOMER,
-      isLoggedIn: GET_IS_LOGGED_IN
+      isLoggedIn: GET_IS_LOGGED_IN,
+      cart: GET_CART
     }),
 
     username(this: Component) {
@@ -22,6 +24,16 @@ export default {
         }
       }
       return ''
+    },
+
+    cartQuantity ( this: Component ) {
+      if ( this['cart'] ) {
+        return this[ 'cart' ].lineItems.reduce( ( quantity, item ) => {
+          return quantity + item.quantity
+        }, 0 )
+      } else {
+        return 0
+      }
     }
   },
 
