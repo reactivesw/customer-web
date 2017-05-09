ee8f65e54af3a47b7039143248ec078ced44a17f
Alan Zhang <gagazcfan@icloud.com>
2017-05-09 10:17:44 +0800

    Api server don't allow localhost CROS now, setup api proxy for local development. (#54)

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ var prodEnv = require('./prod.env')
 module.exports = Object.assign({}, prodEnv, {
   NODE_ENV: '"development"',
 
+  REST_API_URL: '"/api/"', // api proxying during development: http://vuejs-templates.github.io/webpack/proxy.html
   FACEBOOK_APP_ID: process.env.FACEBOOK_LOCAL_APP_ID
 })
