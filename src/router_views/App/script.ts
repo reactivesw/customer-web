9a261e0e47e66ccbff6b2e2b6686c5c32cd898c7
Alan Zhang <gagazcfan@icloud.com>
2017-05-11 10:31:22 +0800

    fix a bug which cause 401 re-login process not triggered properly.

--- src/router_views/App/script.ts
+++ src/router_views/App/script.ts
@@ -7,9 +7,7 @@ import TheFooter from 'src/components/TheFooter'
 import Login from 'src/components/TheHeader/Login'
 import SignUp from 'src/components/TheHeader/SignUp'
 
-import { FETCH_CART } from 'src/infrastructure/store/modules/carts/actions'
-import { GET_CUSTOMER, GET_IS_LOGGED_IN } from 'src/infrastructure/store/modules/auth/getters'
-import { FETCH_CATEGORIES } from 'src/infrastructure/store/modules/categories/actions'
+import { GET_IS_LOGGED_IN } from 'src/infrastructure/store/modules/auth/getters'
 import { SHOW_LOGIN } from 'src/infrastructure/store/modules/modal_dialogs/actions'
 import { LOGOUT } from 'src/infrastructure/store/modules/auth/actions'
 
@@ -24,9 +22,6 @@ import { LOGOUT } from 'src/infrastructure/store/modules/auth/actions'
 export default class App extends Vue {
 
   created() {
-    this.fetchCategories()
-    this.fetchCart()
-
     // Make sure logout current user(outdated) and ask for login again if unAuthorized event raised.
     authApi.setUnAuthorizedListener(async err => {
       const isLoggedIn = this['$store'].getters[GET_IS_LOGGED_IN]
@@ -35,18 +30,5 @@ export default class App extends Vue {
       }
       await this['$store'].dispatch(SHOW_LOGIN)
     })
-  }
-
-  // store operations
-  get customer() {
-    return this.$store.getters[GET_CUSTOMER]
-  }
-
-  fetchCategories() {
-    this.$store.dispatch(FETCH_CATEGORIES)
-  }
-
-  fetchCart() {
-    this.$store.dispatch(FETCH_CART)
   }
 }
