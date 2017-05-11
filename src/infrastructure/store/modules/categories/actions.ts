4cb0e2ff6c403f501653abae309527ffe01dc188
Alan Zhang <gagazcfan@icloud.com>
2017-05-11 10:36:09 +0800

    Ask user to re-login if their token outdated(when server return 401) (#58)
    * Ask user to login when 401 response happend.
    
    * fix a bug which cause 401 re-login process not triggered properly.
    
    * remove unused import

--- src/infrastructure/store/modules/categories/actions.ts
+++ src/infrastructure/store/modules/categories/actions.ts
@@ -1,20 +1,29 @@
 import { categories as categoriesApi } from 'src/infrastructure/api_client'
 import { SET_CATEGORIES } from 'src/infrastructure/store/modules/categories/mutations'
+import { GET_CATEGORIES } from 'src/infrastructure/store/modules/categories/getters'
 
 export const FETCH_CATEGORIES = 'categories/FETCH_CATEGORIES'
 
-// categoriesPromise is for prevent duplicate request and cache.
-let categoriesPromise
 const actions = {
-  async[FETCH_CATEGORIES]({ commit }) {
-    if (!categoriesPromise) {
-      categoriesPromise = categoriesApi.getCategories()
+
+  /**
+   * Fetch categories
+   * Only fetch once unless refresh browser
+   * If categories is fetched, simply return it. (like a async getter)
+   * @param commit
+   * @param getters
+   * @returns {Promise<any>}
+   */
+  async[FETCH_CATEGORIES]({ commit, getters }) {
+    let categories = getters[GET_CATEGORIES]
+    if (categories.length === 0) {
+      categories = await categoriesApi.getCategories()
       .then((categories) => {
         commit(SET_CATEGORIES, categories)
         return categories
       })
     }
-    return await categoriesPromise
+    return categories
   }
 }
 
