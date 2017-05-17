3ec5f618e7943b3dfde541421a3412c19f9e8552
Alan Zhang <gagazcfan@icloud.com>
2017-05-17 16:48:11 +0800

    use vue-awesome-swiper(vue wrapper of swiper) instead of slick-carousel.

--- src/components/product/Gallery/script.ts
+++ src/components/product/Gallery/script.ts
@@ -1,56 +1,31 @@
 import { Component } from 'vue'
-import 'slick-carousel'
-import 'slick-carousel/slick/slick.scss'
-import 'slick-carousel/slick/slick-theme.scss'
-
-const fullsizeOptions = {
-  asNavFor: '.product-gallery-thumbnails',
-  infinite: false,
-  arrows: false,
-}
-
-const thumbnailOptions = {
-  asNavFor: '.product-gallery-fullsizes',
-  centerMode: true,  // put selected slide to the middle
-  variableWidth: true, // no gap between slides
-  infinite: false,  // no cyclic sliding
-  focusOnSelect: true,  // allwo click to select
-}
-
-function initGallery(vm: Component) {
-  const fullsizes = $(vm['$refs'].fullsizes)
-  const thumbnails = $(vm['$refs'].thumbnails)
-
-  // setup fullsizes images slider
-  // slick-carousel docuemnt url https://github.com/kenwheeler/slick/
-  let fullsizesInstance = fullsizes['slick'](fullsizeOptions)
-
-  // setup thumbnails images slider
-  let thumbnailsInstance = thumbnails['slick'](thumbnailOptions)
-}
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 
 export default {
   name: 'Gallery',
 
   props: {
-    slides: Array
+    slides: {
+      type: Array,
+      required: true
+    }
   },
 
-  // to make the jQuery operatoin work, perform three steps
-  // 1. initialize in mounted
-  // 2. undo initialization in beforeUpdate
-  // 3. initialize in updated
-  // 2 and 3 happen whenever there is a prop data change
-  mounted(this: Component) {
-    initGallery(this)
+  data(this: Component) {
+    return {
+      currentImg: this['slides'][0].href, // current selected image
+
+      // options documentation: http://idangero.us/swiper/api/
+      thumbnailOptions: {
+        slidesPerView: 'auto',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+      }
+    }
   },
 
-  beforeUpdate(this: Component) {
-    $(this['$refs'].fullsizes)['slick']('unslick')
-    $(this['$refs'].thumbnails)['slick']('unslick')
-  },
-
-  updated(this: Component) {
-    initGallery(this)
+  components: {
+    swiper,
+    swiperSlide
   }
 }
