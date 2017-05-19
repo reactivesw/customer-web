ef00ceba8da489ff802e16c712c7e8ca37d31d75
Alan Zhang <gagazcfan@icloud.com>
2017-05-19 16:43:04 +0800

    add a SearchResult component

---
+++ src/components/TheHeader/SearchResult/index.vue
@@ -0,0 +1,11 @@
+<template src="./template.html">
+</template>
+
+<script lang="ts">
+// 1. cannot use "src" 2. to export default from default
+import s from './script'
+export default s
+</script>
+
+<style lang="scss" src="./style.scss">
+</style>
